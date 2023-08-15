dab0e3e5809f12890aee085ae7a3e07d22737bfc
minhquan6203 <harryquan.minh@gmail.com>
2023-08-15 19:10:48 +0700

    fix

--- src/data_utils/find_k_sentence.py
+++ src/data_utils/find_k_sentence.py
@@ -1,4 +1,3 @@
-
 from sentence_transformers import util
 import torch
 import json
@@ -6,32 +5,6 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 from underthesea import sent_tokenize
 model = SentenceTransformer('paraphrase-xlm-r-multilingual-v1')
-
-def find_sentence(corpus, start_answer, context):
-    '''
-    Find sentence contains answer
-    * corpus: list sentences of context
-    * start_answer: the begin position of answer in context
-    '''
-    start, end = 0, -1
-    for sentence in corpus:
-        start = end+1
-        end = start+len(sentence)
-        if start<=start_answer<end: break
-    idx_answer_in_sent = start_answer - context.find(sentence)
-    return sentence, idx_answer_in_sent
-
-def update_answer_index(sentence, context_, idx_answer_in_sent):
-    '''
-    Update new answer_start
-    * sentence: sentence contains answer
-    * context_: new context
-    * idx_answer_in_sent: the begin position of answer in sentence 
-    '''
-    i = context_.find(sentence)
-    answer_start_new = i + idx_answer_in_sent
-    return answer_start_new
-
 
 def find_top_k(top_k, model, question, corpus, corpus_embeddings):
     if len(corpus)>top_k:
@@ -69,36 +42,20 @@ def update_data(top_k, data):
         label.append(data['label'][i])
         corpus = sent_tokenize(context)
         corpus_embeddings = model.encode(corpus, convert_to_tensor=True)
-        sentence_answer, idx_answer_in_sent = find_sentence(corpus, start_answer, context)
 
         if len(corpus)>top_k:
-            sentence_new_context = find_top_k(top_k, model, question, corpus, corpus_embeddings)
-            
-            if sentence_answer in sentence_new_context:
-                context_ = ''
-            for pa in corpus:
-                if pa in sentence_new_context: 
-                    context_+= pa+' '
-            context_ = context_[:-1]
+            sentence_new_context = find_top_k(top_k, model, question, corpus, corpus_embeddings) 
+            context_ = ' '.join([pa for pa in corpus if pa in sentence_new_context])
             if answer in context_:
-                start_answer_new = update_answer_index(sentence_answer, context_, idx_answer_in_sent)
-                new_context.append(context_)
-                new_question.append(question)
-                new_answer.append(answer)
-                new_start.append(start_answer_new)
-                new_end.append(start_answer_new+len(answer))
-            else:
-                new_context.append(context)
-                new_question.append(question)
-                new_answer.append(answer)
-                new_start.append(start_answer)
-                new_end.append(end_answer)
-        else:
-            new_context.append(context)
-            new_question.append(question)
-            new_answer.append(answer)
-            new_start.append(start_answer)
-            new_end.append(end_answer)
+                context = context_
+                start_answer = context.find(answer)
+                end_answer  = start_answer + len(answer)
+
+        new_context.append(context)
+        new_question.append(question)
+        new_answer.append(answer)
+        new_start.append(start_answer)
+        new_end.append(end_answer)
 
     new_data={'context':new_context,
             'question':new_question,
@@ -121,17 +78,12 @@ def update_data_test(top_k, data):
         corpus_embeddings = model.encode(corpus, convert_to_tensor=True)
     
         if len(corpus)>top_k:
-            sentence_new_context = find_top_k(top_k, model, question, corpus, corpus_embeddings)
-            context_ = ''
-            for pa in corpus:
-                if pa in sentence_new_context: 
-                    context_+= pa+' '
-            context_ = context_[:-1]
-            new_context.append(context_)
-            new_question.append(question)
-        else:
-            new_context.append(context)
-            new_question.append(question)
+            sentence_new_context = find_top_k(top_k, model, question, corpus, corpus_embeddings) 
+            context_ = ' '.join([pa for pa in corpus if pa in sentence_new_context])
+            context = context_
+
+        new_context.append(context)
+        new_question.append(question)
 
     new_data={'context':new_context,
             'question':new_question}
