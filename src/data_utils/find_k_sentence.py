f49f1babc87f099ef28d0782e455c955dc654dbe
minhquan6203 <harryquan.minh@gmail.com>
2023-10-15 00:31:41 +0700

    fix

--- src/data_utils/find_k_sentence.py
+++ src/data_utils/find_k_sentence.py
@@ -15,89 +15,18 @@ import unicodedata
 # model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
 # model = SentenceTransformer('intfloat/multilingual-e5-large')
 # model = SentenceTransformer('keepitreal/vietnamese-sbert')
-def normalize_text(text):
-    text = text.translate(str.maketrans("", "", string.punctuation))
-    text = text.lower().strip()
-    return text
 
 def drop_last_dot(sentence):
     if sentence[-1] == '.':
         sentence = sentence[:-1]
     return sentence
 
-def get_tokenizer(tokenizer):
-    if callable(tokenizer):
-        return tokenizer
-    elif tokenizer is None:
-        return lambda s: s 
-    elif tokenizer == "pyvi":
-        try:
-            from pyvi import ViTokenizer
-            return ViTokenizer.tokenize
-        except ImportError:
-            print("Please install PyVi package. "
-                  "See the docs at https://github.com/trungtv/pyvi for more information.")
-    elif tokenizer == "spacy":
-        try:
-            from spacy.lang.vi import Vietnamese
-            return Vietnamese()
-        except ImportError:
-            print("Please install SpaCy and the SpaCy Vietnamese tokenizer. "
-                  "See the docs at https://gitlab.com/trungtv/vi_spacy for more information.")
-            raise
-        except AttributeError:
-            print("Please install SpaCy and the SpaCy Vietnamese tokenizer. "
-                  "See the docs at https://gitlab.com/trungtv/vi_spacy for more information.")
-            raise
-    elif tokenizer == "vncorenlp":
-        try:
-            from vncorenlp import VnCoreNLP
-            # before using vncorenlp, please run this command in your terminal:
-            # vncorenlp -Xmx500m data_utils/vncorenlp/VnCoreNLP-1.1.1.jar -p 9000 -annotators wseg &
-            annotator = VnCoreNLP(address="http://127.0.0.1", port=9000, max_heap_size='-Xmx500m')
-
-            def tokenize(s: str):
-                words = annotator.tokenize(s)[0]
-                return " ".join(words)
-
-            return tokenize
-        except ImportError:
-            print("Please install VnCoreNLP package. "
-                  "See the docs at https://github.com/vncorenlp/VnCoreNLP for more information.")
-            raise
-        except AttributeError:
-            print("Please install VnCoreNLP package. "
-                  "See the docs at https://github.com/vncorenlp/VnCoreNLP for more information.")
-            raise
-
-def preprocess_sentence(sentence: str, tokenizer: str=None):
-    sentence = sentence.lower()
-    sentence = unicodedata.normalize('NFC', sentence)
-    sentence = re.sub(r"[“”]", "\"", sentence)
-    sentence = re.sub(r"!", " ! ", sentence)
-    sentence = re.sub(r"\?", " ? ", sentence)
-    sentence = re.sub(r":", " : ", sentence)
-    sentence = re.sub(r";", " ; ", sentence)
-    sentence = re.sub(r",", " , ", sentence)
-    sentence = re.sub(r"\"", " \" ", sentence)
-    sentence = re.sub(r"'", " ' ", sentence)
-    sentence = re.sub(r"\(", " ( ", sentence)
-    sentence = re.sub(r"\[", " [ ", sentence)
-    sentence = re.sub(r"\)", " ) ", sentence)
-    sentence = re.sub(r"\]", " ] ", sentence)
-    sentence = re.sub(r"/", " / ", sentence)
-    sentence = re.sub(r"\.", " . ", sentence)
-    sentence = re.sub(r"-", " - ", sentence)
-    sentence = re.sub(r"\$", " $ ", sentence)
-    sentence = re.sub(r"\&", " & ", sentence)
-    sentence = re.sub(r"\*", " * ", sentence)
-    # tokenize the sentence
-    tokenizer = get_tokenizer(tokenizer)
-    sentence = tokenizer(sentence)
-    sentence = " ".join(sentence.strip().split()) # remove duplicated spaces
-    # tokens = sentence.strip().split()
-    
-    return sentence
+def preprocess_text(text: str) -> str:    
+    text = re.sub(r"['\",\.\?:\-!]", "", text)
+    text = text.strip()
+    text = " ".join(text.split())
+    text = text.lower()
+    return text
 
 def split_sentence(paragraph):
     paragraph=paragraph.replace(':\n\n',': ').strip()
@@ -105,15 +34,9 @@ def split_sentence(paragraph):
     context_list = []
     for context in pre_context_list:
         sen = context.split(". ")
-        context_list = context_list + sen
+        context_list = context_list + [s for s in sen if len(s.split())>1]
+    context_list=[preprocess_text(c) for c in context_list]
     return context_list
-
-# def preprocess_ev(claim,ev):
-#     if len(ev.split())<5:
-#         ev=claim
-#     else:
-#         ev=' '.join(split_sentence(ev))
-#     return ev
 
 class Find_k_sentence:
     def __init__(self):
@@ -146,7 +69,7 @@ class Find_k_sentence:
         for it,i in enumerate(tqdm(range(len(data)))):
             idx=data['idx'][i]
             context = data['context'][i]
-            question = data['question'][i]
+            question = ' '.join(split_sentence(data['question'][i]))
             answer = ' '.join(split_sentence(data['answer'][i]))
             start_answer = data['start'][i]
             end_answer= data['end'][i]
@@ -167,12 +90,12 @@ class Find_k_sentence:
                 start_answer = context.find(answer)
                 end_answer  = start_answer + len(answer) - 1
             else:
-                start_answer = 0
-                end_answer = 0
+                start_answer=len(context)-1
+                end_answer=len(context)-1
 
             new_context.append(context)
             new_question.append(question)
-            new_answer.append(answer)
+            new_answer.append((answer))
             new_start.append(start_answer)
             new_end.append(end_answer)
             all_idx.append(idx)
@@ -221,7 +144,7 @@ def main():
     find_k=Find_k_sentence()
     data=pd.read_csv('./data/data.csv')
     df=find_k.update_data(top_k=5,data=data)
-    df.to_csv('./data/data_new.csv')
+    df.to_csv('./data/data_new.csv',index=False)
 
 if __name__ == '__main__':
     main()
